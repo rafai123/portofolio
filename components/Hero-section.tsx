70dc77fb72eeeb0d51f1c6381d1f8db4f34a3b03
Muhammad Rafai <rafaimhd123@gmail.com>
2024-10-29 22:49:46 +0700

    change nextvul cafe picture

--- components/Hero-section.tsx
+++ components/Hero-section.tsx
@@ -5,7 +5,7 @@ import WordRotate from "./ui/word-rotate"
 import { ArrowBigDownDash } from "lucide-react"
 
 export const HeroSection = () => {
-    
+
     const wordsRotateHome = [
         "Web Developer", 
         "Frontend Web Developer", 
@@ -17,7 +17,7 @@ export const HeroSection = () => {
             <main className="w-full mx-auto max-w-3xl h-[100vh] flex items-center px-7">
                 <div>
                     <BlurFade delay={.25} duration={1.2} inView>
-                        <HyperText duration={1100} className="font-extrabold text-3xl lg:text-4xl border-b-4 border-yellow-600 tracking-widest text-left" text="Muhammad Rafai"/>
+                        <HyperText duration={1000} className="font-extrabold text-3xl lg:text-4xl border-b-4 border-yellow-600 tracking-widest text-left" text="Muhammad Rafai"/>
                     </BlurFade>
                     <BlurFade inView duration={1}>
                         <WordRotate className="font-bold text-3xl lg:text-4xl tracking-widest text-left" words={wordsRotateHome} />
